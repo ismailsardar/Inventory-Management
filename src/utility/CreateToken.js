885bad957c43b18ab3c5a3fbe9e05361f75eaaeb
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 05:34:05 +0600

    Create Token Utility add

--- src/utility/CreateToken.js
+++ src/utility/CreateToken.js
@@ -3,3 +3,12 @@
  * Subject: Inventory Project All package require
  * Auth: Ismile Satdar
  */
+const jwt = require("jsonwebtoken");
+
+const CreateToken = async (data) => {
+  let Payload = {exp: Math.floor(Date.now() / 1000) + (24*60 * 60),data: data};
+
+  return await jwt.sign(Payload, "jg45rt7p87ksd4oap5qv5of");
+};
+
+module.exports = CreateToken;
