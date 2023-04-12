a1b8962f806ad0c3613be29cfb34a02f7e47f107
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 05:49:45 +0600

    User Create Service add

--- src/services/User/UserCreateService.js
+++ src/services/User/UserCreateService.js
@@ -3,3 +3,17 @@
  * Subject: Inventory Project All package require
  * Auth: Ismile Satdar
  */
+
+const UserCreateService = async (request, dataModel) => {
+  try {
+    let postBody = request.body;
+    let data = await dataModel.create(postBody);
+    if (data) {
+      return { status: "success", data: data };
+    }
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = UserCreateService;
