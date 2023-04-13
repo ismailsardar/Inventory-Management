d99343e96d551dc491bf0acefc51c1e910f10b9b
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 20:03:34 +0600

    User Verify Otp Service add

--- src/services/user/UserVerifyOtpService.js
+++ src/services/user/UserVerifyOtpService.js
@@ -2,4 +2,35 @@
  * Date: 13/04/2023
  * Subject: Inventory Project services
  * Auth: Ismile Sardar
-*/
+ */
+
+const UserVerifyOtpService = async (request, dataModel) => {
+  try {
+    // parameter catch
+    let email = request.params.email;
+    let otpCode = request.params.otp;
+    let status = 0;
+    let statusUpdate = 1;
+
+    let otpCount = await dataModel.aggregate([
+      { $match: { email, otp: otpCode, status } },
+      { $count: "total" },
+    ]);
+
+    if (otpCount.length > 0) {
+      //otp update
+      let OTPUpdate = await dataModel.updateOne(
+        { email, otp: otpCode, status },
+        { status: statusUpdate }
+      );
+
+      return { status: "success", data: OTPUpdate };
+    } else {
+      return { status: "fail", data: "Invalid OTP Code!" };
+    }
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = UserVerifyOtpService;
