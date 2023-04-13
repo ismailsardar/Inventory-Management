ab6fffa2231a628238ec1ab20dc3599c40605dd9
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 10:39:14 +0600

    User Login Service add

--- src/utility/CreateToken.js
+++ src/utility/CreateToken.js
@@ -1,6 +1,6 @@
 /**
  * Date: 12/04/2023
- * Subject: Inventory Project All package require
+ * Subject: Inventory Project services
  * Auth: Ismile Satdar
  */
 const jwt = require("jsonwebtoken");
