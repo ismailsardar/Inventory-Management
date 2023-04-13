2eca9419d22cb4bf903db8c9e819e0a483b03874
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 10:09:56 +0600

    User Update Service add

---
+++ src/services/user/UserUpdateService.js
@@ -0,0 +1,22 @@
+/**
+ * Date: 13/04/2023
+ * Subject: Inventory Project services
+ * Auth: Ismile Satdar
+ */
+
+const UserUpdateService = async (request, dataModel) => {
+  try {
+    let data = await dataModel.updateOne(
+      { email: request.headers["email"] },
+      request.body
+    );
+
+    if (data) {
+      return { status: "success", data: data };
+    }
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = UserUpdateService;
