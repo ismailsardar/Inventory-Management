90a806e0dc1c2c557d11413b1b8f013b40209b04
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-13 11:16:33 +0600

    user Schema model

--- src/models/Users/UsersModel.js
+++ src/models/Users/UsersModel.js
@@ -1,5 +1,22 @@
 /**
  * Date: 12/04/2023
- * Subject: Inventory Project All package require
+ * Subject: Inventory Project user model
  * Auth: Ismile Satdar
  */
+
+let mongoose = require("mongoose");
+let userSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    mobile: { type: String },
+    password: { type: String },
+    photo: { type: String },
+    createdDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+let UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
