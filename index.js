b304bdbe3ac7d85eef318bd856f1284d5993280a
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-15 05:56:55 +0600

    sendgrid mail package install

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  */
 
 const app = require("./app");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log("App Run at http://localhost:5000");
+  console.log("App Run at http://localhost:8080");
 });
