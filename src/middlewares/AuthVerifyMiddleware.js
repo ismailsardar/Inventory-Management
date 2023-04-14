4d708e19b2b0391db4971388fe395de8ccd910eb
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-15 05:59:05 +0600

    User Login Service projection update

--- src/middlewares/AuthVerifyMiddleware.js
+++ src/middlewares/AuthVerifyMiddleware.js
@@ -7,8 +7,8 @@
 let jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  let token = req.headers["token"];
-  jwt.verify(token, "jg45rt7p87ksd4oap5qv5of", (error, decoded) => {
+  let Token = req.headers["token"];
+  jwt.verify(Token, "jg45rt7p87ksd4oap5qv5of", (error, decoded) => {
     if (error) {
       res.status(401).json({ status: "UnAuthorized" });
     } else {
