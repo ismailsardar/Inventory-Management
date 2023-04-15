444eaa2db52c4391ebf0c96d54c123ce7ebdc427
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-16 05:50:49 +0600

    Brands Categories Customers Suppliers api create

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,6 +11,14 @@ const router = express.Router();
 const AuthVerifyMiddleware = require('../middlewares/AuthVerifyMiddleware');
 // Users Controller modules
 const UsersController = require('../controllers/Users/UsersController');
+// Brand Controller modules
+const BrandsControllers = require('../controllers/Brands/BrandsControllers');
+// Categories Controller modules
+const CategoriesControllers = require('../controllers/Categories/CategoriesControllers');
+// Customers Controller modules
+const CustomersControllers = require('../controllers/Customers/CustomersControllers');
+// Suppliers Controller modules
+const SuppliersControllers = require('../controllers/Suppliers/SuppliersControllers');
 
 // testing router
 router.get("/", (req, res) => {
@@ -25,5 +33,29 @@ router.get("/profileDetails",AuthVerifyMiddleware,UsersController.ProfileDetails
 router.get("/recoverEmail/:email",UsersController.RecoverVerifyEmail);
 router.get("/verifyOtp/:email/:otp",UsersController.RecoverVerifyOTP);
 router.post("/resetPassword",UsersController.resetPassword );
+
+// Brand Routing
+router.post("/createBrand",AuthVerifyMiddleware,BrandsControllers.CreateBrand);
+router.post("/updateBrand/:id",AuthVerifyMiddleware,BrandsControllers.UpdateBrand);
+router.get("/brandList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,BrandsControllers.BrandList);
+router.get("/brandDropDown",AuthVerifyMiddleware,BrandsControllers.BrandDropDown);
+
+// Categories Routing
+router.post("/createCategories",AuthVerifyMiddleware,CategoriesControllers.CreateCategories);
+router.post("/updateCategories/:id",AuthVerifyMiddleware,CategoriesControllers.UpdateCategories);
+router.get("/categoriesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CategoriesControllers.CategoriesList);
+router.get("/categoriesDropDown",AuthVerifyMiddleware,CategoriesControllers.CategoriesDropDown);
+
+// Customers Routing
+router.post("/createCustomers",AuthVerifyMiddleware,CustomersControllers.CreateCustomers);
+router.post("/updateCustomers/:id",AuthVerifyMiddleware,CustomersControllers.UpdateCustomers);
+router.get("/customersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CustomersControllers.CustomersList);
+router.get("/customersDropDown",AuthVerifyMiddleware,CustomersControllers.CustomersDropDown);
+
+// Suppliers Routing
+router.post("/createSuppliers",AuthVerifyMiddleware,SuppliersControllers.CreateSuppliers);
+router.post("/updateSuppliers/:id",AuthVerifyMiddleware,SuppliersControllers.UpdateSuppliers);
+router.get("/suppliersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SuppliersControllers.SuppliersList);
+router.get("/suppliersDropDown",AuthVerifyMiddleware,SuppliersControllers.SuppliersDropDown);
 
 module.exports = router;
