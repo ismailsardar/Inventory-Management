70b8bc35fb56dd63d0aacef28290c1b0a9c5cf85
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-15 06:01:26 +0600

    users handler all api add

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,9 +7,23 @@
 const express = require("express");
 const router = express.Router();
 
+//Auth Verify Middleware
+const AuthVerifyMiddleware = require('../middlewares/AuthVerifyMiddleware');
+// Users Controller modules
+const UsersController = require('../controllers/Users/UsersController');
+
 // testing router
 router.get("/", (req, res) => {
   res.send("This is tasting Router");
 });
+
+// users Router
+router.post("/registration",UsersController.Registration);
+router.post("/login",UsersController.Login);
+router.post("/profileUpdate",AuthVerifyMiddleware,UsersController.ProfileUpdate);
+router.get("/profileDetails",AuthVerifyMiddleware,UsersController.ProfileDetails);
+router.get("/recoverEmail/:email",UsersController.RecoverVerifyEmail);
+router.get("/verifyOtp/:email/:otp",UsersController.RecoverVerifyOTP);
+router.post("/resetPassword",UsersController.resetPassword );
 
 module.exports = router;
