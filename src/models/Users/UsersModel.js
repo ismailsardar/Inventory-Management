bbaa41347ef3f5732ce090c59e4a6de4b6f2029b
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-16 05:45:47 +0600

    User Model update

--- src/models/Users/UsersModel.js
+++ src/models/Users/UsersModel.js
@@ -7,7 +7,7 @@
 let mongoose = require("mongoose");
 let userSchema = new mongoose.Schema(
   {
-    email: { type: String, unique: true },
+    UserEmail: { type: String, unique: true },
     firstName: { type: String },
     lastName: { type: String },
     mobile: { type: String },
