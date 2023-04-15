1f13db47b80fa97887a47dceee738e8d7ee371a2
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-16 05:44:56 +0600

    User Login Service update

--- src/services/user/UserLoginService.js
+++ src/services/user/UserLoginService.js
@@ -10,11 +10,11 @@ const UserLoginService = async (request, dataModel) => {
     try {
       let data = await dataModel.aggregate([
         { $match: request.body },
-        {$project:{_id:0,email:1,firstName:1,lastName:1,mobile:1}}
+        {$project:{_id:0,UserEmail:1,firstName:1,lastName:1,mobile:1}}
       ]);
   
       if (data.length > 0) {
-        let token = await CreateToken(data[0]['email']);
+        let token = await CreateToken(data[0]['UserEmail']);
         return { status: "success", data: data,token };
       }else{
         return {status:"unauthorized"}
