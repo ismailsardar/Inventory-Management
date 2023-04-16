c092d51eccc54ddd142b7748a6ac84e3245e0106
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-16 19:17:10 +0600

    update expense Controller

--- src/controllers/Expense/expenseControllers.js
+++ src/controllers/Expense/expenseControllers.js
@@ -9,17 +9,30 @@ const ExpenseModel = require("../../models/Expense/ExpensesModel");
 
 // common services import
 const CreateService = require("../../services/common/createService");
+const ListOneJoinService = require("../../services/common/listOneJoinService");
 const UpdateService = require("../../services/common/updateService");
 
-// CreateBrand
+// CreateExpense
 exports.CreateExpense = async (req, res) => {
   let result = await CreateService(req, ExpenseModel);
   res.status(201).json(result);
 };
 
-// UpdateBrand
+// UpdateExpense
 exports.UpdateExpense = async (req, res) => {
   let result = await UpdateService(req, ExpenseModel);
   res.status(200).json(result);
 };
 
+// BrandList
+exports.ExpenseList = async (req, res) => {
+  let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+  let SearchArray = [
+    { Amount: SearchRgx },
+    { Note: SearchRgx },
+    { "Type.Name": SearchRgx },
+  ];
+  let JoinStage = {$lookup: {from: "expensetypes", localField: "TypeID", foreignField: "_id", as: "Type"}}
+  let result = await ListOneJoinService(req, ExpenseModel, SearchArray,JoinStage);
+  res.status(200).json(result);
+};
