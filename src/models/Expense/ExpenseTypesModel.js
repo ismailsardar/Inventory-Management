48658aba884d540d4543252ec8986b8279a4d53b
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-16 06:04:37 +0600

    create expenseTypeSchema

---
+++ src/models/Expense/ExpenseTypesModel.js
@@ -0,0 +1,20 @@
+/**
+ * Date: 16/04/2023
+ * Subject: Inventory Project user model
+ * Auth: Ismile Satdar
+ */
+
+const mongoose = require("mongoose");
+
+const expenseTypeSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    Name: { type: String, unique: true },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const ExpenseModel = mongoose.model("expensetypes", expenseTypeSchema);
+
+module.exports = ExpenseModel;
