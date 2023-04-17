d7e4dc196be38435b5c587ce8a49efa58e685a77
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-17 12:27:01 +0600

    update Purchases List controller

--- src/controllers/Purchase/purchaseControllers.js
+++ src/controllers/Purchase/purchaseControllers.js
@@ -8,8 +8,9 @@
 const ParentModel = require("../../models/Purchases/PurchasesModel");
 const ChildeModel = require("../../models/Purchases/PurchaseProductsModel");
 const ParentChildeService = require("../../services/common/createParentChildsService");
+const ListOneJoinService = require("../../services/common/listOneJoinService");
 
-// CreatePurchases
+// Create Purchases
 exports.CreatePurchases = async (req, res) => {
   let Result = await ParentChildeService(
     req,
@@ -18,4 +19,31 @@ exports.CreatePurchases = async (req, res) => {
     "PurchaseID"
   );
   res.status(200).json(Result);
+};
+
+// Purchases List
+exports.PurchasesList = async (req, res) => {
+  let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+  let SearchArray = [
+    { Note: SearchRgx },
+    { "suppliers.Name": SearchRgx },
+    { "suppliers.Address": SearchRgx },
+    { "suppliers.Phone": SearchRgx },
+    { "suppliers.Email": SearchRgx },
+  ];
+  let JoinStage = {
+    $lookup: {
+      from: "suppliers",
+      localField: "SupplierID",
+      foreignField: "_id",
+      as: "suppliers",
+    },
+  };
+  let result = await ListOneJoinService(
+    req,
+    ParentModel,
+    SearchArray,
+    JoinStage
+  );
+  res.status(200).json(result);
 };
