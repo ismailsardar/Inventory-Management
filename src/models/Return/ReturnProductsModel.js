45345f071c93ead0a94999c2a16a8ab01c1e4e08
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 05:29:49 +0600

    create ReturnProductsModel

---
+++ src/models/Return/ReturnProductsModel.js
@@ -0,0 +1,26 @@
+
+/**
+ * Date: 17/04/2023
+ * Subject: Inventory sales purchases model
+ * Auth: Ismile Satdar
+ */
+
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const returnProductsSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    ReturnID: { type: ObjectId },
+    ProductID: { type: ObjectId },
+    Qty: { type: Number },
+    UnitCost: { type: Number },
+    Total: { type: Number },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const ReturnProductsModel = mongoose.model("returnproducts",returnProductsSchema);
+
+module.exports = ReturnProductsModel;
