a0655aea28abe23cd5e2831923ced14d87282c2e
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-17 12:09:53 +0600

    crate ParentChildeService

---
+++ src/services/common/createParentChildsService.js
@@ -0,0 +1,55 @@
+/**
+ * Date: 17/04/2023
+ * Subject: Inventory Project purchase services
+ * Auth: Ismile Satdar
+ */
+
+const mongoose = require("mongoose");
+
+const ParentChildeService = async (
+  req,
+  ParentModel,
+  ChildeModel,
+  JoinPropertyName
+) => {
+  // Create Transaction Session
+  const session = await mongoose.startSession();
+
+  try {
+    // Begin Transaction Session
+    session.startTransaction();
+
+    // First DataBase Process
+    let Parent = req.body["Parent"];
+    Parent.UserEmail = req.headers["email"];
+    let ParentCreation = await ParentModel.create([Parent], { session });
+
+    // Second Database Process
+    let Childe = req.body["Childe"];
+    await Childe.forEach((ele) => {
+      ele[JoinPropertyName] = ParentCreation[0]["_id"];
+      ele["UserEmail"] = req.headers["email"];
+    });
+    // Data inserts
+    let ChildeCreation = await ChildeModel.insertMany(Childe, { session });
+
+    // Transaction success
+    await session.commitTransaction();
+    session.endSession();
+
+    // response
+    return {
+      status: "success",
+      parent: ParentCreation,
+      childe: ChildeCreation,
+    };
+  } catch (error) {
+    console.log(error)
+    // Roll Back Transaction if Fail
+    await session.abortTransaction();
+    session.endSession();
+    return { status: "fail", data: error.message };
+  }
+};
+
+module.exports = ParentChildeService;
