48437f69263f94e07f9bd400191d2d0f6b904644
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-17 12:10:30 +0600

    create PurchaseProductsModel

---
+++ src/models/Purchases/PurchaseProductsModel.js
@@ -0,0 +1,26 @@
+/**
+ * Date: 17/04/2023
+ * Subject: Inventory Project purchases model
+ * Auth: Ismile Satdar
+ */
+
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const purchaseProductsSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    PurchaseID: { type: ObjectId },
+    ProductID: { type: ObjectId },
+    Qty: { type: Number },
+    UnitCost: { type: Number },
+    Total: { type: Number },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const PurchaseProductsModel = mongoose.model("purchaseproducts",purchaseProductsSchema
+);
+
+module.exports = PurchaseProductsModel;
