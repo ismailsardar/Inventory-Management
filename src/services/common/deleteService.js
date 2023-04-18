10af5ed59130a24b55b0a474cf3147ce9009a665
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 09:34:51 +0600

    create Delete Service

---
+++ src/services/common/deleteService.js
@@ -0,0 +1,28 @@
+/**
+ * Date: 18/04/2023
+ * Subject: Inventory Project purchase services
+ * Auth: Ismile Satdar
+ */
+
+const DeleteService = async (req, Model) => {
+  try {
+    let DeleteID = req.params.id;
+    let UserEmail = req.headers["email"];
+
+    // Second Process
+    let QueryObj = {};
+    QueryObj["_id"] = DeleteID;
+    QueryObj[UserEmail] = UserEmail;
+
+    // First DataBase Process
+    let Delete = await Model.deleteMany(QueryObj);
+
+    // response
+    return {status: "success", delete: Delete};
+
+  } catch (error) {
+    return { status: "fail", data: error.message };
+  }
+};
+
+module.exports = DeleteService;
