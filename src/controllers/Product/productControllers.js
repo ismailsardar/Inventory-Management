7aa7ffae3cfc4044860cef35f7b04abd465ad634
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 09:38:00 +0600

    update Product controller

--- src/controllers/Product/productControllers.js
+++ src/controllers/Product/productControllers.js
@@ -5,40 +5,101 @@
  */
 
 //data base import
+const { mongoose } = require("mongoose");
 const ProductModel = require("../../models/Products/ProductModule");
 
 // common services import
 const CreateService = require("../../services/common/createService");
 const ListTwoJoinService = require("../../services/common/listTwoJoinService");
 const UpdateService = require("../../services/common/updateService");
+const DeleteService = require("../../services/common/deleteService");
+const CheckAssociateService = require("../../services/common/checkAssociateService");
+const SaleProductsModel = require("../../models/Sales/SalesProductsModel");
+const ReturnProductsModel = require("../../models/Return/ReturnProductsModel");
+const PurchaseProductsModel = require("../../models/Purchases/PurchaseProductsModel");
 
-// CreateExpense
+// Create Product
 exports.CreateProduct = async (req, res) => {
   let result = await CreateService(req, ProductModel);
   res.status(201).json(result);
 };
 
-// UpdateExpense
+// Update Product
 exports.UpdateProduct = async (req, res) => {
   let result = await UpdateService(req, ProductModel);
   res.status(200).json(result);
 };
 
-// BrandList
+// Product List
 exports.ProductList = async (req, res) => {
   let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
   let SearchArray = [
-    {Name: SearchRgx},
-    {Unit: SearchRgx},
-    {Details: SearchRgx},
-    {'brands.Name':SearchRgx},
-    {'categories.Name':SearchRgx}
-];
-  let JoinStage1 = {$lookup: {from: "brands", localField: "BrandID", foreignField: "_id", as: "brands"}}
+    { Name: SearchRgx },
+    { Unit: SearchRgx },
+    { Details: SearchRgx },
+    { "brands.Name": SearchRgx },
+    { "categories.Name": SearchRgx },
+  ];
+  let JoinStage1 = {
+    $lookup: {
+      from: "brands",
+      localField: "BrandID",
+      foreignField: "_id",
+      as: "brands",
+    },
+  };
 
-  let JoinStage2 = {$lookup: {from: "categories", localField: "CategoryID", foreignField: "_id", as: "categories"}}
+  let JoinStage2 = {
+    $lookup: {
+      from: "categories",
+      localField: "CategoryID",
+      foreignField: "_id",
+      as: "categories",
+    },
+  };
 
-  let result = await ListTwoJoinService(req, ProductModel, SearchArray,JoinStage1,JoinStage2);
-  
+  let result = await ListTwoJoinService(
+    req,
+    ProductModel,
+    SearchArray,
+    JoinStage1,
+    JoinStage2
+  );
+
   res.status(200).json(result);
+};
+
+// Delete Product
+exports.DeleteProduct = async (req, res) => {
+  let DeleteId = req.params.id;
+  let { ObjectId } = mongoose.Types;
+  let CheckAssociate = await CheckAssociateService(
+    { ProductID: ObjectId(DeleteId) },
+    PurchaseProductsModel
+  );
+  let CheckAssociate1 = await CheckAssociateService(
+    { ProductID: ObjectId(DeleteId) },
+    SaleProductsModel
+  );
+  let CheckAssociate2 = await CheckAssociateService(
+    { ProductID: ObjectId(DeleteId) },
+    ReturnProductsModel
+  );
+
+  if (CheckAssociate) {
+    res
+      .status(200)
+      .json({ status: "associate", data: "Associate with Product" });
+  } else if (CheckAssociate1) {
+    res
+      .status(200)
+      .json({ status: "associate", data: "Associate with Product" });
+  } else if (CheckAssociate2) {
+    res
+      .status(200)
+      .json({ status: "associate", data: "Associate with Product" });
+  } else {
+    let result = await DeleteService(req, CustomersModel);
+    res.status(200).json(result);
+  }
 };
