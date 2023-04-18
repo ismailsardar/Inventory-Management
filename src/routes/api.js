0f4f5477dd177b3d1da0421dc260dbfd54b9737c
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 09:40:15 +0600

    update api file

--- src/routes/api.js
+++ src/routes/api.js
@@ -51,51 +51,61 @@ router.post("/createBrand",AuthVerifyMiddleware,BrandsControllers.CreateBrand);
 router.post("/updateBrand/:id",AuthVerifyMiddleware,BrandsControllers.UpdateBrand);
 router.get("/brandList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,BrandsControllers.BrandList);
 router.get("/brandDropDown",AuthVerifyMiddleware,BrandsControllers.BrandDropDown);
+router.get("/deleteBrand/:id",AuthVerifyMiddleware,BrandsControllers.DeleteBrand);
 
 // Categories Routing
 router.post("/createCategories",AuthVerifyMiddleware,CategoriesControllers.CreateCategories);
 router.post("/updateCategories/:id",AuthVerifyMiddleware,CategoriesControllers.UpdateCategories);
 router.get("/categoriesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CategoriesControllers.CategoriesList);
 router.get("/categoriesDropDown",AuthVerifyMiddleware,CategoriesControllers.CategoriesDropDown);
+router.get("/deleteCategories/:id",AuthVerifyMiddleware,CategoriesControllers.DeleteCategories);
 
 // Customers Routing
 router.post("/createCustomers",AuthVerifyMiddleware,CustomersControllers.CreateCustomers);
 router.post("/updateCustomers/:id",AuthVerifyMiddleware,CustomersControllers.UpdateCustomers);
 router.get("/customersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CustomersControllers.CustomersList);
 router.get("/customersDropDown",AuthVerifyMiddleware,CustomersControllers.CustomersDropDown);
+router.get("/deleteCustomers/:id",AuthVerifyMiddleware,CustomersControllers.DeleteCustomers);
 
 // Suppliers Routing
 router.post("/createSuppliers",AuthVerifyMiddleware,SuppliersControllers.CreateSuppliers);
 router.post("/updateSuppliers/:id",AuthVerifyMiddleware,SuppliersControllers.UpdateSuppliers);
 router.get("/suppliersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SuppliersControllers.SuppliersList);
 router.get("/suppliersDropDown",AuthVerifyMiddleware,SuppliersControllers.SuppliersDropDown);
+router.get("/deleteSupplier/:id",AuthVerifyMiddleware,SuppliersControllers.DeleteSuppliers);
 
 // Expense Types Routing
 router.post("/createExpenseType",AuthVerifyMiddleware,ExpenseType.CreateExpenseTypes);
 router.post("/updateExpenseType/:id",AuthVerifyMiddleware,ExpenseType.UpdateExpenseTypes);
 router.get("/expenseTypeList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ExpenseType.ExpenseTypesList);
 router.get("/expenseTypeDropDown",AuthVerifyMiddleware,ExpenseType.ExpenseTypesDropDown);
+router.get("/deleteExpenseType/:id",AuthVerifyMiddleware,ExpenseType.DeleteExpenseTypes);
 
 // Expense Routing
 router.post("/createExpense",AuthVerifyMiddleware,Expenses.CreateExpense);
 router.post("/updateExpense/:id",AuthVerifyMiddleware,Expenses.UpdateExpense);
 router.get("/expenseList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,Expenses.ExpenseList);
+router.get("/deleteExpense/:id",AuthVerifyMiddleware,Expenses.DeleteExpense);
 
 // Product Routing
 router.post("/createProduct",AuthVerifyMiddleware,ProductController.CreateProduct);
 router.post("/updateProduct/:id",AuthVerifyMiddleware,ProductController.UpdateProduct);
 router.get("/productList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ProductController.ProductList);
+router.get("/deleteProduct/:id",AuthVerifyMiddleware,ProductController.DeleteProduct);
 
 // Purchases Routing
 router.post("/createPurchases",AuthVerifyMiddleware,PurchasesController.CreatePurchases);
 router.get("/purchasesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,PurchasesController.PurchasesList);
+router.get("/deletePurchase/:id",AuthVerifyMiddleware,PurchasesController.DeletePurchases);
 
 // Sales
 router.post("/createSales",AuthVerifyMiddleware,SalesController.CreateSales);
 router.get("/salesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SalesController.SalesList);
+router.get("/deleteSales/:id",AuthVerifyMiddleware,SalesController.DeleteSales);
 
 // Return Routing
 router.post("/createReturn",AuthVerifyMiddleware,ReturnController.CreateReturn);
 router.get("/returnList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,ReturnController.ReturnList);
+router.get("/deleteReturn/:id",AuthVerifyMiddleware,ReturnController.DeleteReturn);
 
 module.exports = router;
