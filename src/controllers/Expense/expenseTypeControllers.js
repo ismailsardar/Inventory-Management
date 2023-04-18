e6b18730f8bff6816b152c4f0c3b217abf3eedbe
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 09:38:25 +0600

    Delete Expense Types controller

--- src/controllers/Expense/expenseTypeControllers.js
+++ src/controllers/Expense/expenseTypeControllers.js
@@ -5,6 +5,7 @@
  */
 
 //data base import
+const { mongoose } = require("mongoose");
 const ExpenseTypesModel = require("../../models/Expense/ExpenseTypesModel");
 
 // common services import
@@ -12,20 +13,23 @@ const CreateService = require("../../services/common/createService");
 const DropDownService = require("../../services/common/dropDownService");
 const ListService = require("../../services/common/listService");
 const UpdateService = require("../../services/common/updateService");
+const DeleteService = require("../../services/common/deleteService");
+const CheckAssociateService = require("../../services/common/checkAssociateService");
+const ExpenseModel = require("../../models/Expense/ExpensesModel");
 
-// CreateBrand
+// Create ExpenseTypes
 exports.CreateExpenseTypes = async (req, res) => {
   let result = await CreateService(req, ExpenseTypesModel);
   res.status(201).json(result);
 };
 
-// UpdateBrand
+// Update ExpenseTypes
 exports.UpdateExpenseTypes = async (req, res) => {
   let result = await UpdateService(req, ExpenseTypesModel);
   res.status(200).json(result);
 };
 
-// BrandList
+// ExpenseTypes List
 exports.ExpenseTypesList = async (req, res) => {
   let SearchRex = { $regex: req.params.searchKeyword, $options: "i" };
   let SearchArray = [{ Name: SearchRex }];
@@ -33,8 +37,23 @@ exports.ExpenseTypesList = async (req, res) => {
   res.status(200).json(result);
 };
 
-// BrandDropDown
+// ExpenseTypes DropDown
 exports.ExpenseTypesDropDown = async (req, res) => {
   let result = await DropDownService(req, ExpenseTypesModel, { _id: 1, Name: 1 });
   res.status(200).json(result);
+};
+
+
+// Delete ExpenseTypes
+exports.DeleteExpenseTypes = async (req, res) => {
+  let DeleteId = req.params.id;
+  let {ObjectId} = mongoose.Types;
+  let CheckAssociate = await CheckAssociateService({TypeID:ObjectId(DeleteId)},ExpenseModel);
+
+  if (CheckAssociate) {
+    res.status(200).json({status: "associate", data: "Associate with Product"});
+  } else {
+    let result = await DeleteService(req, ExpenseTypesModel)
+    res.status(200).json(result);
+  }
 };
