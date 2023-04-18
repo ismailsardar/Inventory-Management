8a4050f1e870c30ee23be6f5a45d8d01aafdbaf5
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-18 09:39:11 +0600

    add Delete Customers function

--- src/controllers/Customers/CustomersControllers.js
+++ src/controllers/Customers/CustomersControllers.js
@@ -5,6 +5,7 @@
  */
 
 //data base import
+const { mongoose } = require("mongoose");
 const CustomersModel = require("../../models/Customers/CustomersModel");
 
 // common services import
@@ -12,20 +13,23 @@ const CreateService = require("../../services/common/createService");
 const DropDownService = require("../../services/common/dropDownService");
 const ListService = require("../../services/common/listService");
 const UpdateService = require("../../services/common/updateService");
+const DeleteService = require("../../services/common/deleteService");
+const CheckAssociateService = require("../../services/common/checkAssociateService");
+const SalesModel = require("../../models/Sales/SalesModel");
 
-// CreateBrand
+// Create Customers
 exports.CreateCustomers = async (req, res) => {
   let result = await CreateService(req, CustomersModel);
   res.status(201).json(result);
 };
 
-// UpdateBrand
+// Update Customers
 exports.UpdateCustomers = async (req, res) => {
   let result = await UpdateService(req, CustomersModel);
   res.status(200).json(result);
 };
 
-// BrandList
+// Customers List
 exports.CustomersList = async (req, res) => {
   let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
   let SearchArray=[{CustomerName: SearchRgx},{Phone: SearchRgx},{Email: SearchRgx},{Address: SearchRgx}]
@@ -33,8 +37,22 @@ exports.CustomersList = async (req, res) => {
   res.status(200).json(result);
 };
 
-// BrandDropDown
+// Customers DropDown
 exports.CustomersDropDown = async (req, res) => {
   let result = await DropDownService(req, CustomersModel, { _id: 1,CustomerName: 1 });
   res.status(200).json(result);
+};
+
+// Delete Categories
+exports.DeleteCustomers = async (req, res) => {
+  let DeleteId = req.params.id;
+  let {ObjectId} = mongoose.Types;
+  let CheckAssociate = await CheckAssociateService({CustomerID:ObjectId(DeleteId)},SalesModel);
+
+  if (CheckAssociate) {
+    res.status(200).json({status: "associate", data: "Associate with Product"});
+  } else {
+    let result = await DeleteService(req, CustomersModel)
+    res.status(200).json(result);
+  }
 };
