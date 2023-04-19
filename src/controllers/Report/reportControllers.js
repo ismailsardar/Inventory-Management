dbc9bf1e042b60441eb784bf1d0b5b3a79d0a503
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-19 20:03:13 +0600

    create Report controller

---
+++ src/controllers/Report/reportControllers.js
@@ -0,0 +1,34 @@
+/**
+ * Date: 18/04/2023
+ * Subject: Inventory Project purchase controller
+ * Auth: Ismile Satdar
+ */
+
+const ExpenseReportService = require("../../services/Reports/ExpenseReportService");
+const PurchasesReportService = require("../../services/Reports/PurchasesReportService");
+const ReturnReportService = require("../../services/Reports/ReturnReportService");
+const SalesReportService = require("../../services/Reports/SalesReportService");
+
+// Expenses Report By Date
+exports.ExpensesByDate = async (req, res) => {
+  let result = await ExpenseReportService(req);
+  return res.status(200).json(result);
+};
+
+// Return Report By Date
+exports.ReturnByDate = async (req, res) => {
+  let result = await ReturnReportService(req);
+  return res.status(200).json(result);
+};
+
+// Purchase Report By Date
+exports.PurchaseByDate = async (req, res) => {
+  let result = await PurchasesReportService(req);
+  return res.status(200).json(result);
+};
+
+// Sales Report By Date
+exports.SalesByDate = async (req, res) => {
+  let result = await SalesReportService(req);
+  return res.status(200).json(result);
+};
