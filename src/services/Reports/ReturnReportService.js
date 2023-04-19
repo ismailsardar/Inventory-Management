53bfa15f42fe4d786594359b5f681527772790ae
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-19 20:00:40 +0600

    create Return Report Service

---
+++ src/services/Reports/ReturnReportService.js
@@ -0,0 +1,61 @@
+/**
+ * Date: 19  /04/2023
+ * Subject: Inventory Project purchase services Report
+ * Auth: Ismile Satdar
+ */
+
+const ReturnProductsModel = require("../../models/Return/ReturnProductsModel");
+
+const ReturnReportService = async (req) => {
+  try {
+    let UserEmail = req.headers["email"];
+    let { FormDate, ToDate } = req.body;
+
+    const data = await ReturnProductsModel.aggregate([
+      {
+        $match: {
+          UserEmail: UserEmail,
+          CreatedDate: { $gte: new Date(FormDate), $lte: new Date(ToDate) },
+        },
+      },
+      {
+        $facet: {
+          Total: [{ $group: { _id: 0, TotalAmount: { $sum: "$Total" } } }],
+          Rows: [
+            {
+              $lookup: {
+                from: "products",
+                localField: "ProductID",
+                foreignField: "_id",
+                as: "products",
+              },
+            },
+            { $unwind: "$products" },
+            {
+              $lookup: {
+                from: "brands",
+                localField: "products.BrandID",
+                foreignField: "_id",
+                as: "brands",
+              },
+            },
+            {
+              $lookup: {
+                from: "categories",
+                localField: "products.CategoryID",
+                foreignField: "_id",
+                as: "categories",
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    return { status: "success", data: data };
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = ReturnReportService;
