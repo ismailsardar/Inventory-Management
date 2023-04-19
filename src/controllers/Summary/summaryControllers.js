f47477794bf3fe0b0eab57920825e03d4b297ef9
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-19 20:02:48 +0600

    create Summary Controller

---
+++ src/controllers/Summary/summaryControllers.js
@@ -0,0 +1,34 @@
+/**
+ * Date: 19/04/2023
+ * Subject: Inventory Project Summary controller
+ * Auth: Ismile Satdar
+ */
+
+const ExpenseSummaryService = require("../../services/Summary/ExpenseSummaryService");
+const PurchaseSummaryService = require("../../services/Summary/PurchaseSummaryService");
+const ReturnSummaryService = require("../../services/Summary/ReturnSummaryService");
+const SalesSummaryService = require("../../services/Summary/SalesSummaryService");
+
+// ExpensesSummary
+exports.ExpensesSummary=async (req, res) => {
+    let Result=await ExpenseSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// ReturnSummary
+exports.ReturnSummary=async (req, res) => {
+    let Result=await ReturnSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// PurchaseSummary
+exports.PurchaseSummary=async (req, res) => {
+    let Result=await PurchaseSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// SalesSummary
+exports.SalesSummary=async (req, res) => {
+    let Result=await SalesSummaryService(req)
+    res.status(200).json(Result);
+}
