7a698012af5a0975318d483ab1f0fb1bbd0cb28f
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-19 19:59:49 +0600

    create Expense Summary Service

---
+++ src/services/Summary/ExpenseSummaryService.js
@@ -0,0 +1,43 @@
+/**
+ * Date: 19/04/2023
+ * Subject: Inventory Project Summary Report Service
+ * Auth: Ismile Satdar
+ */
+
+const ExpenseModel = require("../../models/Expense/ExpensesModel");
+
+const ExpenseSummaryService = async (req) => {
+  try {
+    let UserEmail = req.headers["email"];
+
+    let data = await ExpenseModel.aggregate([
+      {
+        $match: {
+          UserEmail: UserEmail,
+        },
+      },
+      {
+        $facet: {
+          Total: [{ $group: { _id: 0, TotalAmount: { $sum: "$Amount" } } }],
+          Last30Days: [
+            {
+              $group: {
+                _id: {
+                  $dateToString: { format: "%Y-%m-%d", date: "$CreatedDate" },
+                },
+                TotalAmount: { $sum: "$Amount" },
+              },
+            },
+            { $sort: { _id: -1 } },
+            { $limit: 30 },
+          ],
+        },
+      },
+    ]);
+    return { status: "success", data: data };
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = ExpenseSummaryService;
