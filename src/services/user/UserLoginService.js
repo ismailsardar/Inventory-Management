f5db22ab0b633df7ab57deb4108343a3ccf6cd09
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-23 12:58:14 +0600

    UserLoginService update

--- src/services/user/UserLoginService.js
+++ src/services/user/UserLoginService.js
@@ -10,7 +10,7 @@ const UserLoginService = async (request, dataModel) => {
     try {
       let data = await dataModel.aggregate([
         { $match: request.body },
-        {$project:{_id:0,UserEmail:1,firstName:1,lastName:1,mobile:1}}
+        {$project:{_id:0,UserEmail:1,firstName:1,lastName:1,mobile:1,photo:1}}
       ]);
   
       if (data.length > 0) {
